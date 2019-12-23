8a013ee8cc85fe3f5f12ecd5ca56aff98374437d
ElioPez <jetasagames@gmail.com>
2019-12-23 14:36:40 -0300

    Modifique la migracion de permisos para probar

--- database/migrations/2016_02_26_154323_entrust_setup_tables.php
+++ database/migrations/2016_02_26_154323_entrust_setup_tables.php
@@ -24,13 +24,13 @@ class EntrustSetupTables extends Migration
         Schema::create('role_user', function (Blueprint $table) {
             $table->integer('user_id')->unsigned();
             $table->integer('role_id')->unsigned();
+            $table->primary(['user_id', 'role_id']);
 
             $table->foreign('user_id')->references('id')->on('users')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')
                 ->onUpdate('cascade')->onDelete('cascade');
 
-            $table->primary(['user_id', 'role_id']);
         });
 
         // Create table for storing permissions
